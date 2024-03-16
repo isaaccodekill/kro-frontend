9a38b4984963176438dc309c47577568c25df9b1
isaacbello <isaac@teachino.io>
2024-03-16 15:36:07 +0100

    handle tailwind issue with clashing styles

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,27 +1,59 @@
+'use client';
 
-import { Input, Button } from "antd";
+import { Input, Button, Form, type FormProps } from "antd";
+
+type FieldTypes = {
+    email: string;
+    password: string;
+}
 export default function Page() {
+
+const onFinish = (values: FieldTypes) => {
+    console.log("Success:", values);
+}
+
+const onFinishFailed = (errorInfo: FormProps<FieldTypes>) => {
+    console.log("Failed:", errorInfo);
+}
+
+const initialValues = {
+    email: "",
+    password: ""
+}
+
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <main className="flex min-h-screen flex-col items-center p-24">
         <h1 className="text-4xl font-bold">Login</h1>
-        <form className="flex flex-col items-center justify-between p-24">
-            <Input
-                size={"large"}
-            type="email"
-            placeholder="Email"
-            />
-            <Input
-                size={"large"}
-            type="password"
-            placeholder="Password"
-            />
-            <Button type="primary"
-            className="w-full"
-                    size={"large"}
+        <Form name="basic" layout="vertical" initialValues={initialValues} onFinish={onFinish} className="mt-10 w-full" style={{ maxWidth: 400 }}>
+            <Form.Item<FieldTypes>
+                label="Email"
+                name="email"
+                className="flex flex-col"
+                rules={[{ required: true, message: 'Please input your email!' }]}
             >
-            Login
-            </Button>
-        </form>
+                <Input
+                    type="email"
+                    size={"large"}
+                    placeholder="Email"
+                />
+            </Form.Item>
+            <Form.Item<FieldTypes>
+                label="Password"
+                name="password"
+                rules={[{ required: true, message: 'Please input your password!' }]}
+            >
+                <Input
+                    type="password"
+                    size={"large"}
+                    placeholder="Password"
+                />
+            </Form.Item>
+            <Form.Item>
+                <Button type="primary" htmlType="submit" className="w-full">
+                    Log in
+                </Button>
+            </Form.Item>
+        </Form>
         </main>
     );
 }
